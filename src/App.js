cc548b2c3d76a4aa1815bd52832b61a4f3d90fac
Carlos <thehandyowl.io@gmail.com>
2017-12-13 17:54:50 +0100

    Cambiamos la ruta de entrada moviendo App.js a la carpeta src

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    const title='Bienvenido';
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {title}!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
