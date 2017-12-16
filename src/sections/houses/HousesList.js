5f17b0eded848fd115b3fb6a97347c18e45641ae
Carlos <thehandyowl.io@gmail.com>
2017-12-17 00:09:36 +0100

    Hacemos axios.get y obtenemos el listado, que sacamos por consola

--- src/sections/houses/HousesList.js
+++ src/sections/houses/HousesList.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import axios from 'axios'
+
 export default class HousesList extends Component {
+
+    componentWillMount(){
+        axios.get('http://146.185.137.85/got/web/casas')
+        .then( function (response) {
+            console.log("axios get response: ", response);
+        })
+        .catch( function (error) {
+            console.log("axios get error: ", error);
+        })
+    }
 
     render() {
         texto='Listado de casas';
