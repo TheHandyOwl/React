a047308789a4cacb3b4b86e88c4ff41c97dccb1c
Carlos <thehandyowl.io@gmail.com>
2017-12-17 00:33:23 +0100

    Iniciamos el estado con la lista, pero cambiamos las funciones a arrowFunction para mantener el contexto al recibir la respuesta.

--- src/sections/houses/HousesList.js
+++ src/sections/houses/HousesList.js
@@ -5,18 +5,31 @@ import axios from 'axios'
 
 export default class HousesList extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            list: [],
+            selected: null
+        }
+    }
+
     componentWillMount(){
         axios.get('http://146.185.137.85/got/web/casas')
-        .then( function (response) {
+        .then( (response) => {
             console.log("axios get response: ", response);
+            const list = response.data && response.data.records ? response.data.records : []
+            this.setState( { list: list } )
         })
-        .catch( function (error) {
+        .catch( (error) => {
             console.log("axios get error: ", error);
+            return []
         })
     }
 
     render() {
         texto='Listado de casas';
+        console.log("Recuperamos this.state.list: ", this.state.list);
 
         return (
             <View>
