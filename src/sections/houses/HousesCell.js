1698933033e0d75b617a3adb5baaa2f82a33a9bc
Carlos <thehandyowl.io@gmail.com>
2017-12-17 20:04:51 +0100

    Refactor: sacando la celda del FlatList y cambiando el botón por un Touchable. Comunicación entre componentes

---
+++ src/sections/houses/HousesCell.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet , TouchableOpacity } from 'react-native'
+import { Colors } from 'RepasoParaProbar/src/commons';
+
+export default class HousesCell extends Component {
+
+    render() {
+        const { item, index, onSelectItem } = this.props
+        
+        return (
+            <View>
+                <TouchableOpacity
+                    style={ styles.button }
+                    onPress={ () => onSelectItem(item) }
+                >
+                    <Text>{ index }.- { item.nombre }</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    button: {
+        borderColor: Colors.grey,
+        borderWidth: 3,
+        padding: 10,
+        margin: 20,
+        borderRadius: 12,
+    }
+    
+});
--- src/sections/houses/HousesList.js
+++ src/sections/houses/HousesList.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { AsyncCalls, Colors } from 'RepasoParaProbar/src/commons';
+
+import HousesCell from './HousesCell'
 
 export default class HousesList extends Component {
 
@@ -9,7 +11,7 @@ export default class HousesList extends Component {
 
         this.state = {
             list: [],
-            selected: null
+            selected: null,
         }
     }
 
@@ -25,44 +27,25 @@ export default class HousesList extends Component {
             })
     }
 
-    checkIsSelected(item) {
-        if( (this.state.selected != null) && (this.state.selected.id == item.id) ) {
-            return true
-        } else {
-            return false
-        }
-    }
-
-    onSelectedItem(item) {
-
-        this.setState( { selected: item } )
+    onSelectItem(house) {
+        console.log("house: ", house)
+        this.setState({ selected: house })
     }
 
     renderItem(item, index) {
-        const isSelected = this.checkIsSelected(item)
-        const cellStyle = isSelected ? { backgroundColor: Colors.red } : { backgroundColor: Colors.yellow }
-        const titleStyle = isSelected ? { color: Colors.yellow } : { color: Colors.red }
-        const titleColor = isSelected ? Colors.yellow : Colors.red
-        
-        return(            
-            <View style={ [styles.cell, cellStyle] }>
-                <Text style={ titleStyle }>{ index }.- { item.nombre }</Text>
-                <Button
-                    title={item.nombre}
-                    onPress={ () => { this.onSelectedItem(item) } }
-                    color={titleColor}
-                />
-            </View>
+        return (
+            <HousesCell
+                item={item}
+                index={index}
+                onSelectItem={ (house) => this.onSelectItem(house) }
+            />
         )
     }
 
     render() {
-        const nombre = this.state.selected && this.state.selected.nombre ?
-            this.state.selected.nombre : 'Seleccione una casa'
 
         return (
-            <View>
-                <Text style={styles.title}>{ nombre }</Text>
+            <View style={styles.container}>
                 <FlatList
                     data={this.state.list}
                     renderItem={ ( { item, index } ) => this.renderItem(item, index) }
@@ -78,14 +61,8 @@ export default class HousesList extends Component {
 
 const styles = StyleSheet.create({
 
-    cell: {
-        height: 100,
-        marginVertical: 10
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-        marginVertical: 20
+    container: {
+        flex: 1,
     }
     
 });
