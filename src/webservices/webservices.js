d2e154f6d5adc753fec618f8e5deeae35cc9a5cd
Carlos <thehandyowl.io@gmail.com>
2017-12-17 17:58:03 +0100

    Método POST y POST alternativo implementados, aunque sin uso

--- src/webservices/webservices.js
+++ src/webservices/webservices.js
@@ -58,3 +58,37 @@ export function fetchAlternativo(url) {
 
     });
 }
+
+export function post(url, data) {
+    return new Promise ( function(resolve, reject) {
+        axios.get(url)
+            .then( (response) => {
+                console.log("webservie axios post response: ", response);
+
+                if(response.data) {
+                    resolve ( response.data )
+                } else {
+                    reject ( response )
+                }
+            })
+            .catch( (error) => {
+                reject ( error )
+            })
+        })
+}
+
+export function postAlternativo(url, data) {
+    return axios.post(url, data).then((response) => {
+
+        return response.data
+
+    }).catch((error) => {
+
+        if (error.response) {
+            throw { code: error.response.status, msg: error.response.data, error: error }
+        } else {
+            throw { code: 500, msg: error.message, error: error }
+        }
+
+    });
+}
