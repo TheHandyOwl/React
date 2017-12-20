1aa1b7122b0929339cfe39875c5758dbf2321d5d
Carlos <thehandyowl.io@gmail.com>
2017-12-20 01:34:37 +0100

    Creamos la vista CharactersList

---
+++ src/sections/characters/CharactersList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import { FlatList, StyleSheet, View } from 'react-native'
+
+export default class CharactersList extends Component {
+
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    
+});
