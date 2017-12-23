9554c98f54257c25b58a1b8ddcd540a98a30b044
Carlos <thehandyowl.io@gmail.com>
2017-12-23 02:02:52 +0100

    Creamos elementos y damos estilo a la vista. Y enseguida la conectaremos a Redux para coger los valores del estado

--- src/sections/characters/CharacterDetail.js
+++ src/sections/characters/CharacterDetail.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native'
+
+import { Colors } from 'RepasoParaProbar/src/commons'
 
 export default class CharacterDetail extends Component {
 
+    onSubmit() {
+        console.log("Ha puslado el botón Eliminar")
+    }
+
     render() {
+        const { character } = this.props
+        const nombre = ''
+        const edad = ''
+        const image = null
+
         return (
-            <View>
-                <Text>Detail</Text>
+            <View style={styles.container}>
+
+                <Image source={image} style={styles.image} resizeMode={'cover'} /> 
+                <View style={styles.textContainer}>
+                    <Text style={styles.name}>{ nombre }</Text>
+                    <Text style={styles.edad}>{ 'Edad: ' + edad }</Text>
+                </View>
+
+                <View style={styles.buttonContainer}>
+                    <Button title={'Eliminar'} onPress={ () => this.onSubmit(character) } isFetching={this.props.isFetching} />
+                </View>
+
             </View>
         )
     }
@@ -14,9 +35,37 @@ export default class CharacterDetail extends Component {
 }
 
 const styles = StyleSheet.create({
-
+    
     container: {
         flex: 1,
-    }
-    
-})
+        backgroundColor: Colors.background,
+    },
+
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 20,
+    },
+
+    name: {
+        flex: 1,
+        fontSize: 20,
+        fontWeight: '600',
+        color: Colors.white,
+    },
+
+    edad: {
+        fontSize: 16,
+        color: Colors.white,
+    },
+
+    image: {
+        width: Dimensions.get('window').width,
+        height: 200,
+    },
+
+    buttonContainer: {
+        margin: 20,
+    },
+
+});
