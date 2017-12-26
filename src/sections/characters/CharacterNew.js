ccc8337304638f301d96ff9ad8200b51498a61bb
Carlos <thehandyowl.io@gmail.com>
2017-12-27 00:01:09 +0100

    Personalizamos el input con una caja de texto y la mostramos desde la vista padre. Se aplican estilos

--- src/sections/characters/CharacterNew.js
+++ src/sections/characters/CharacterNew.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
 import { Colors } from 'RepasoParaProbar/src/commons'
+import { Input } from 'RepasoParaProbar/src/widgets'
 
 // Redux
 import { connect } from 'react-redux';
@@ -9,10 +10,27 @@ import * as CharactersActions from 'RepasoParaProbar/src/redux/actions/character
 
 class CharacterNew extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name        : '',
+            nameError   : '',
+        }
+
+    }
+
     render() {
+        console.log("this.state.name: ", this.state.name)
         return (
             <View style={styles.container}>
-                <Text>CharacterNew</Text>
+                <Input
+                    onChangeText    = { (v) => this.setState( { name: v } )}
+                    value           = { this.state.name }
+                    error           = { this.state.nameError }
+                    label           = { 'Nombre:' }
+                    placeholder     = { 'Eddard Stark' }
+                />
             </View>
         )
     }
@@ -33,8 +51,47 @@ const mapDispatchToProps = (dispatch, props) => {
 export default connect (null, null) (CharacterNew)
 
 const styles = StyleSheet.create({
+
     container: {
-      flex: 1,
-      backgroundColor: Colors.background,
+        flex: 1,
+        backgroundColor: Colors.background,
+        padding: 20,
     },
+
+    imageContainer: {
+        width: '100%',
+        height: 200,
+        backgroundColor: 'grey',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    imageContainerBackground: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0
+    },
+
+    button: {
+        padding: 10,
+        borderColor: 'white',
+        borderWidth: 1,
+        borderRadius: 6,
+    },
+
+    textButton: {
+        color: 'white',
+        fontWeight: '600',
+    },
+
+    inputContainer: {
+        margin: 20,
+    },
+
+    buttonContainer: {
+        margin: 20,
+    },
+
 })
--- src/widgets/Input.js
+++ src/widgets/Input.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 
+import { Colors } from 'RepasoParaProbar/src/commons'
+
 export default class Input extends Component {
 
     static defaultProps = {
-        labelStyle: {},
-        containerStyle: {},
-        spinnerColor: 'white',
-        label: '',
-        onPress: () => {},
-        isFetching: false,
+        labelStyle  : {},
+        inputStyle  : {},
+        errorStyle  : {},
+        label       : '',
+        value       : '',
+        error       : '',
+        placeholder : '',
+        onChangeText: () => {},
     }
 
     render() {
         return (
             <View style={styles.container}>
+                <Text
+                    style={[styles.label, this.props.labelStyle]}>
+                    {this.props.label}
+                </Text>
+                <TextInput
+                    value                   = { this.props.value }
+                    onChangeText            = { (v) => this.props.onChangeText(v) }
+                    placeholder             = { this.props.placeholder }
+                    placeholderTextColor    = { Colors.grey }
+                    style                   = { [styles.input, this.props.inputStyle] }
+                    underlineColorAndroid   = { 'transparent' }
+                />
+
+                {
+                    this.props.error ?
+                        <Text style={[styles.error, this.props.errorStyle]}>
+                            {this.props.error}
+                        </Text>
+                        : null
+                }
             </View>
         )
     }
@@ -26,4 +50,25 @@ const styles = StyleSheet.create({
 
     },
 
+    error: {
+        color: Colors.white,
+        textAlign: 'right',
+        marginTop: 4,
+    },
+
+    input: {
+        borderColor: Colors.grey,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 6,
+        fontSize: 16,
+        color: Colors.white,
+    },
+
+    label: {
+        color: Colors.white,
+        fontSize: 16,
+        marginBottom: 10,
+        fontWeight: '600',
+    },
 })
